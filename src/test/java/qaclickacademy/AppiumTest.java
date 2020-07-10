a9d5c424e49bec645f254ec507195a0d912d4297
prasad <prasadsarode1@gmail.com>
2020-07-10 19:14:26 +0530

    architect YYY new code in Appium

--- src/test/java/qaclickacademy/AppiumTest.java
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -8,12 +8,19 @@ public class AppiumTest {
 	public void NativeAppAndroid()
 	{
 		System.out.println("NativeAppAndroid");
+		
+		System.out.println("YYY is adding some tests now in develop");
 	}
 	
 	@Test
 	public void IOSApps()
 	{
 		System.out.println("IOSApps");
+		
+		
+		System.out.println("YYY is adding some tests now in develop");
+		System.out.println("YYY is adding some tests now in develop");
+		System.out.println("YYY is adding some tests now in develop");
 	}
 
 }
