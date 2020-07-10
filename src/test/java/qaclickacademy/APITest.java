84d83928f2fc8b8bff04739d8c0195b439252129
prasad <prasadsarode1@gmail.com>
2020-07-10 17:06:59 +0530

    XXX second commit

--- src/test/java/qaclickacademy/APITest.java
+++ src/test/java/qaclickacademy/APITest.java
@@ -11,12 +11,18 @@ public class APITest {
 		
 		System.out.println("I am adding a new code I am XXX");
 		System.out.println("did you get my code YYY please reply");
+		
+		
+		System.out.println("now I am XXX going to add nw code are you ready YYY");
+		System.out.println("now I am XXX here I come again");
 	}
 	
 	@Test
 	public void deleteTwitter()
 	{
 		System.out.println("deleteTwitter");
+		
+		System.out.println("i XXX made changes");
 	}
 
 }
